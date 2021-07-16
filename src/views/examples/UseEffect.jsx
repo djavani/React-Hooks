6df9dbef16fd3fe1e0765233a2dcfada0e2d6d84
Djavani <djavanirgomes@gmail.com>
2021-07-16 20:27:34 -0300

    UseEffect e UseState

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,13 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from './../../components/layout/SectionTitle';
+
+
+function calcFatorial(num) {
+    const n = parseInt(num)
+    if (n < 0) return -1
+    if (n === 0) return 1
+    return calcFatorial(n - 1) * n
+}
 
 const UseEffect = (props) => {
+
+    // Ex #01
+    const [number, setNumber] = useState(1)
+    const [fatorial, setFatorial] = useState(1)    
+
+    useEffect(function () {
+        setFatorial(calcFatorial(number))
+    }, [number])
+
+    
+    useEffect(function () {
+        document.title = fatorial > 10000000 ? 'Eita Doido!' : 'Tá de  Boa'
+    }, [fatorial])
+
+    // Ex #02
+    const [status, setStatus] = useState("Impar")
+
+    useEffect(function () {
+        setStatus(number % 2 === 0 ? "PAR" : "IMPAR")
+    }, [number])
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
+                subtitle="Permite executar efeitos colaterais em componentes funcionais!" />
+
+            <SectionTitle title="Exercicio #1"/>
+            <div className="center">
+                <div>
+                    <span className="text">Fatorial:</span>
+                    <span className="text red">{fatorial === -1 ? 'Nao existe' : fatorial}</span>
+                </div>
+                <input type="number" className="input"
+                    value={number}
+                    onChange={e => setNumber(e.target.value)} />
+            </div>
+            <SectionTitle title="Exercicio #2" />
+            <div className="center">
+                <div>
+                   <span className="text">Status:</span> 
+                   <span className="text red">{status}</span>
+                </div>
+            </div>
         </div>
     )
 }
--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from './../../components/layout/SectionTitle';
 
 const UseState = (props) => {
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState("Inicial...")
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
+                subtitle="Estado em componentes funcionais!" />
+
+            <SectionTitle title="Exercicio #01" />
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <buton className="btn"
+                    onClick={() => setCount(count -1)}>-1</buton>
+                    <buton className="btn"
+                    onClick={() => setCount(count +1)}>+1</buton>
+                    <buton className="btn"
+                    onClick={() => setCount(100)}>100</buton>
+                    <buton className="btn"
+                    onClick={() => setCount(atual => atual + 1000)}>+1000</buton>
+                </div>
+            </div>
+            <SectionTitle title="Exercicio #02" />
+            <input type="text" className="input" 
+            value={name} onChange={e => setName(e.target.value)}/>
         </div>
     )
 }
