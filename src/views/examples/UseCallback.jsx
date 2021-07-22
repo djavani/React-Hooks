90339718918559d65c44d379a85dda283fb61ccc
Djavani <djavanirgomes@gmail.com>
2021-07-22 16:52:16 -0300

    Hook useCallback

--- src/views/examples/UseCallback.jsx
+++ src/views/examples/UseCallback.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import UseCallbackButtons from './UseCallbackButtons'
 
+/*
+em uma pagina com dois componentes (pai e filho) passando uma
+funcao de callback para setar a renderizacao apenas do componente
+que esta sofrendo modificacao (contador), não é necessário renderizar
+o componente Botoes a cada novo valor do contador, por isso foi passado 
+como argumento da funcao o setCount, pois é instanciado uma unica vez
+*/
 const UseCallback = (props) => {
+
+    const [count, setCount] = useState(0)
+
+    const inc = useCallback(function (delta) {
+        setCount(curr => curr + delta)
+    }, [setCount])
+   
+
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
-                subtitle="Retorna uma função memoizada!"
-            />
+                subtitle="Retorna uma função memoizada!" />
+
+            <div className="center">
+                <div className="center">
+                    <span className="text">{count}</span>
+                </div>
+
+                <div>
+                    <UseCallbackButtons inc={inc} />
+                </div>
+            </div>
         </div>
     )
 }
